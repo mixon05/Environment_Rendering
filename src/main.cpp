2eadd067414978adab2a5f1ac7f87e09dfc92e2d
bjazwiec <bjazwiec.stud@pw.edu.pl>
2024-12-07 23:20:55 +0100

    z-buffer

--- src/main.cpp
+++ src/main.cpp
@@ -102,10 +102,14 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+
+    // Włączamy Z-buffer
+    glEnable(GL_DEPTH_TEST);  
+
     // Główna pętla renderowania
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
 
