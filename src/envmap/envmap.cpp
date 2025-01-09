42f4ca485b8d1b79f7c5017825a3e704a2c493ec
bjazwiec <bjazwiec.stud@pw.edu.pl>
2025-01-10 00:13:56 +0100

    poprawka

--- src/envmap/envmap.cpp
+++ src/envmap/envmap.cpp
@@ -25,7 +25,7 @@ EnvMap::EnvMap(std::string filepath, float yScale, float xStride, float zStride)
     for(int i = 0; i < zSize; i++){
         for(int j = 0; j< xSize; j++){
             
-            vertices[3 * (i * xSize + j)] = xStride * (j - (xSize -1) / 2.0);
+            vertices[3 * (i * xSize + j)] = xStride * (j - (xSize - 1) / 2.0);
             vertices[3 * (i * xSize + j) + 1] = data[(i * xSize + j) * channels] / yScale;
             vertices[3 * (i * xSize + j) + 2] = zStride * (i - (zSize - 1) / 2.0);
 
@@ -34,17 +34,19 @@ EnvMap::EnvMap(std::string filepath, float yScale, float xStride, float zStride)
 
     stbi_image_free(data);
 
-    unsigned int indices_size = 6*(xSize-1)*(zSize-1);
+    unsigned int indices_size = 3*2*(xSize-1)*(zSize-1);
     this->indices = new unsigned int [indices_size];
-    for(int i = 0; i < (zSize-1); i++) {
-        for(int j = 0; j < (xSize-1); j++) {
+    for(int i = 0; i < zSize-1; i++) {
+        for(int j = 0; j < xSize-1; j++) {
 
-            indices[6*(i*(xSize-1) + j)] = i*(xSize) + j;
-            indices[6*(i*(xSize-1) + j) + 1] = i*(xSize) + j + 1;
+            indices[6*(i*(xSize-1) + j)] = i*(xSize) + j + 1;
+            indices[6*(i*(xSize-1) + j) + 1] = i*(xSize) + j;
             indices[6*(i*(xSize-1) + j) + 2] = (i+1)*(xSize) + j + 1;
-            indices[6*(i*(xSize-1) + j) + 3] = i*(xSize) + j;
+            indices[6*(i*(xSize-1) + j) + 3] = (i)*(xSize) + j;
             indices[6*(i*(xSize-1) + j) + 4] = (i+1)*(xSize) + j;
             indices[6*(i*(xSize-1) + j) + 5] = (i+1)*(xSize) + j + 1;
+
+
         }
     }
 
